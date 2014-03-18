34ea2e67580db0a94a5479b03d2150474c1948e5
Elliott Biondo <elliott.biondo@gmail.com>
2014-03-18 17:19:01 -0500

    Starting MOAB sampling

--- sampling.cpp
+++ sampling.cpp
@@ -14,23 +14,53 @@
  * Homepage:  apps.jcns.fz-juelich.de/ransampl
  */
 
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <vector>
 
-class aliasTable{
+#include <iostream>
+#include <sstream>
+#include <iomanip> // for setprecision
+#include <limits> // for min/max values
+#include <math.h>
+#include <vector>
 
-public:
+#include "moab/Core.hpp"
+#include "MBTagConventions.hpp"
+#include "moab/Range.hpp"
+#include "moab/Core.hpp"
+
+//MBInterface *MBI();
+
+
+/*
+std::vector<double> pdfFromIMesh(imesh, char* tagName){
+
+}
+
+std::vector<double> pdfFromIMesh(imesh, char* tagName, char* biasTagName){
+
+}
+
+std::vector<double> pdfFromHDF5(hdf5, char* tagName, char* biasTagName){
+
+}
+
+std::vector<double> pdfFromHDF5(hdf5, char* tagName){
+
+}
+*/
+
+class AliasTable{
   std::vector<double> prob;
   std::vector<int> alias;
   int n;
-
-  aliasTable(std::vector<double> pIn);
+public:
+  AliasTable(std::vector<double> p);
   int drawSample(double ran1, double ran2);
 };
 
-aliasTable::aliasTable(std::vector<double> p){
+AliasTable::AliasTable(std::vector<double> p){
     
     n = p.size();
     prob.resize(n);
@@ -85,19 +115,17 @@ aliasTable::aliasTable(std::vector<double> p){
         prob[ S[--nS] ] = 1;
 }
 
-int aliasTable::drawSample(double ran1, double ran2){
+int AliasTable::drawSample(double ran1, double ran2){
     int i = (int) n * ran1;
     return ran2 < prob[i] ? i : alias[i];
 }
-
-
 
 int main(){
 
   double my_array[5] = {1.0, 2.0, 3.0, 4.0, 5.0};
   std::vector<double> my_vec(&my_array[0], &my_array[0]+5);
 
-  aliasTable myTable(my_vec);
+  AliasTable myTable(my_vec);
 
   int answers[] = {0, 0, 0, 0, 0};
   int N = 1000000;
@@ -113,5 +141,15 @@ int main(){
   for(i=0; i<5; i++){
     printf("%i    %f   %f \n", i+1, (float) answers[i]/N, (float) (i+1)/15.0);
   }
+
+  moab::Core *mb;
+  moab::ErrorCode rval = mb->load_mesh("test.h5m");
+  moab::Tag phtnSrcTag;
+
   return 0;
 }
+
+//MBInterface *MBI(){
+//    static MBCore instance;
+//    return &instance;
+//}
