5113296a311d777bfdfec09a78637217a831e139
Elliott Biondo <elliott.biondo@gmail.com>
2014-03-19 16:14:01 -0500

    Int, double, double vector tags access all works.

--- sampling.cpp
+++ sampling.cpp
@@ -129,70 +129,45 @@ int main()
     printf("%i    %f   %f \n", i+1, (double) answers[i]/N, (double) (i+1)/15.0);
   }
 
-  //moab::Core *mb;
   moab::Interface *mb = new moab::Core;
   moab::ErrorCode rval;
   rval = mb->load_mesh("test.h5m");
   moab::Range hex;
   rval = mb->get_entities_by_type( 0, moab::MBHEX, hex );
-
   moab::Tag idxTag;
   moab::Tag phtnSrcTag;
   moab::Tag phtnSrcTag2;
 
-  //rval = mb->tag_get_handle( "ve_idx", 0, tag_type, idxTag, moab::MB_TAG_ANY);
-  rval = mb->tag_get_handle( "idx", 0, moab::MB_TYPE_INTEGER, idxTag);
-  rval = mb->tag_get_handle( "phtn_src", 0, moab::MB_TYPE_DOUBLE, phtnSrcTag);
-  rval = mb->tag_get_handle( "phtn_src2", 0, moab::MB_TYPE_DOUBLE, phtnSrcTag2);
+  rval = mb->tag_get_handle( "idx", moab::MB_TAG_VARLEN, moab::MB_TYPE_INTEGER, idxTag);
+  rval = mb->tag_get_handle( "phtn_src", moab::MB_TAG_VARLEN, moab::MB_TYPE_DOUBLE, phtnSrcTag);
+  rval = mb->tag_get_handle( "phtn_src2", moab::MB_TAG_VARLEN, moab::MB_TYPE_DOUBLE, phtnSrcTag2);
 
   int idxTagSize;
   int phtnSrcTagSize;
   int phtnSrcTagSize2;
 
-  rval = mb->tag_get_bytes(idxTag, idxTagSize);
+  rval = mb->tag_get_bytes(idxTag, *(&idxTagSize));
   rval = mb->tag_get_bytes(phtnSrcTag, *(&phtnSrcTagSize));
   rval = mb->tag_get_bytes(phtnSrcTag2, *(&phtnSrcTagSize2));
 
-  std::cout<< phtnSrcTagSize << std::endl;
-  std::cout<< phtnSrcTagSize2 << std::endl;
+  std::vector<int> idxData;
+  idxData.resize(hex.size()*idxTagSize/sizeof(int)); 
 
-  //moab::DataType tag_type;
-  //rval = mb->tag_get_data_type(idxTag, tag_type);
-
-  std::vector<int>* idxData = new std::vector<int> [idxTagSize * hex.size() / sizeof(int)];
-  std::vector<double>* phtnSrcData = new std::vector<double> [phtnSrcTagSize * hex.size() / sizeof(double)];
-//  std::vector<double>* phtnSrcData2 = new std::vector<double> [phtnSrcTagSize2 * (hex.size() + 2) / sizeof(double)];
-
-  //std::vector<double>* phtnSrcData2 = new std::vector<double> [phtnSrcTagSize2 * hex.size() / 4];
+  std::vector<double> phtnSrcData;
+  phtnSrcData.resize(hex.size()*phtnSrcTagSize/sizeof(double)); 
 
   std::vector<double> phtnSrcData2;
   phtnSrcData2.resize(hex.size()*phtnSrcTagSize2/sizeof(double)); 
- //void* ptr = &phtnSrcData2[0];
 
-//printf("\n hex.size(): %i", (int) hex.size());
-//printf("\n vector length: %i double %i", (int) phtnSrcTagSize2, (int) sizeof(double));
-
-  rval = mb->tag_get_data( idxTag, hex, idxData);
-  rval = mb->tag_get_data( phtnSrcTag, hex, phtnSrcData);
+  rval = mb->tag_get_data( idxTag, hex, &idxData[0]);
+  rval = mb->tag_get_data( phtnSrcTag, hex, &phtnSrcData[0]);
   rval = mb->tag_get_data( phtnSrcTag2, hex, &phtnSrcData2[0]);
-
-  //std::cout << phtnSrcData2.size() <<std::endl;
   
-//  for(i=0; i<hex.size(); ++i){
-//    printf("\n idx: %i phtn_src: %f", ((int*)(idxData))[i], ((double*)(phtnSrcData))[i]);
-//  }
-
-  for(i=0; i<hex.size()*2; ++i){
-    //printf("\n phtn_src2: %f",phtnSrcData2[i]);
-    std::cout << phtnSrcData2[i] << std::endl;
+  for(i=0; i<hex.size(); ++i){
+    std::cout << idxData[i] <<" "<< phtnSrcData[i] <<" "<<phtnSrcData2[2*i]<<" "<< phtnSrcData2[2*i+1] <<" "<< std::endl;
   }
   
-  //std::vector<moab::EntityHandle> ents; 
   return 0;
 }
 
-//moab::MBInterface *MBI(){
-//    static MBCore instance; 
-//    return &instance;
-//}
 
